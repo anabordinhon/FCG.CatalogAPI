5336fae7c8c61d81f50b21c70dd24962c82a989b
Daiana Russi <daianarussi.agroconsult@gmail.com>
2026-01-02 23:33:55 -0300

    feat: tests

---
+++ FCG.Catalog/FCG.Catalog.Tests/Domain/Common/ValueObjects/PriceTests.cs
@@ -0,0 +1,44 @@
+using Bogus;
+using FCG.Catalog.Domain.Common.ValueObjects;
+using FluentAssertions;
+
+namespace FCG.Catalog.Tests.Domain.Common.ValueObjects
+{
+    public class PriceTests
+    {
+        private readonly Faker _faker;
+
+        public PriceTests()
+        {
+            _faker = new Faker("pt_BR");
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenPriceIsNegative()
+        {
+            // Arrange
+            decimal negativePrice = _faker.Random.Decimal(-1000, -0.01m);
+
+            // Act
+            Action act = () => Price.Create(negativePrice);
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+               .WithMessage("O preço não pode ser negativo");
+        }
+
+        [Fact]
+        public void Create_ShouldReturnPrice_WhenValueIsValid()
+        {
+            // Arrange
+            decimal validPrice = _faker.Random.Decimal(0.01m, 10000m);
+
+            // Act
+            var price = Price.Create(validPrice);
+
+            // Assert
+            price.Should().NotBeNull();
+            price.Value.Should().Be(validPrice);
+        }
+    }
+}
